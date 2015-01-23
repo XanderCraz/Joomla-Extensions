c359fcee5dba2177a2e42433fff3beb1ba97cd18
subashbasnet8 <subashbsnt8@gmail.com>
2015-01-23 22:11:36 +0545

    JEDChecker Errors Corrected

--- modules/mod_sbfootscores/mod_sbfootscores.php
+++ modules/mod_sbfootscores/mod_sbfootscores.php
@@ -1,10 +1,10 @@
 <?php
 /**
-  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+defined('_JEXEC') or die('access denied');
 
 // Include the syndicate functions only once
 require_once __DIR__ . '/helper.php';
@@ -12,4 +12,5 @@ ModSbFootScoresHelper::loadDocuments();
 $com_id = $params->get('com_name');
 $result = ModSbFootScoresHelper::init('http://www.football-data.org/alpha/soccerseasons/'.$com_id.'/leagueTable');
 $data = json_decode($result,true);
+
 require JModuleHelper::getLayoutPath('mod_sbfootscores', $params->get('layout', 'ranking'));
--- modules/mod_sbfootscores/tmpl/ranking.php
+++ modules/mod_sbfootscores/tmpl/ranking.php
@@ -1,5 +1,10 @@
-<?php echo "<pre>";
-//print_r($data);die;?>
+<?php 
+/**
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('access denied');
+?>
 
 <div id="ranking_list">
     <table class="table table-condensed">
